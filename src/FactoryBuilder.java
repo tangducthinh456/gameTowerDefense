063d1685a482e9dabf181b9ab0898cb658f613db
thinhtd <tangducthinh456@gmail.com>
2019-11-12 22:46:35 +0700

    chia package

--- src/FactoryBuilder.java
+++
@@ -1,9 +0,0 @@
-
-public class FactoryBuilder
-{
-    static int LEVEL = 1;
-    //while (true)
-    //{
-    //    if (GameField.timeCount % 50 == 0) GameField.enemyList.add(GameCreate.drawSmallerEnemy());
-    //}
-}
